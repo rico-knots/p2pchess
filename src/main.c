eaa0a080769b3ec2774ba1ecfe6c5e182e9a28b3
Rico <enricovdarend@gmail.com>
2026-07-30 00:53:41 +0200

    draw piece from game state func, fixed a bug where black and white textures were the same sheet

--- src/main.c
+++ src/main.c
@@ -1,13 +1,15 @@
+#include "engine/bitboard.h"
+#include "engine/chess.h"
 #include "ui/window.h"
 
 int main() {
     if (window_init(1280, 720) < 0) return 1;
 
+    GameState game_state = engine_new_game();
+
     while (!window_should_close()) {
         window_begin_frame();
-
-        
-        
+        window_draw(&game_state);
         window_end_frame();
     }
 
